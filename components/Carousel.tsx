d585fb3d7daf8979728c2a3bca661bca30958c63
shchoi2 <shchoi2@netpas.net>
2022-12-06 15:20:55 +0900

    Fix: delete unnecessary

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,5 +1,3 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import styled from "styled-components";
 import Image from "next/image";
